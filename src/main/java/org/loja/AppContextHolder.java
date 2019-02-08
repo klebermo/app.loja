b7cbd632810a474341a9f72644ba230f5dbf60b2
Kleber Mota <klebermo@gmail.com>
2019-02-08 10:29:59 -0300

    AppContextHolder.java

---
+++ src/main/java/org/loja/AppContextHolder.java
@@ -0,0 +1,22 @@
+package org.loja;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppContextHolder implements ApplicationContextAware {
+
+	private static ApplicationContext context;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context = applicationContext;
+	}
+
+	public static ApplicationContext getContext() {
+        return context;
+    }
+
+}
