497e7b9f88c0f5febe06996a89b7a62286896547
Kleber Mota <klebermo@gmail.com>
2019-02-27 17:46:01 -0300

    model Credencial

--- src/main/java/org/loja/model/credencial/Credencial.java
+++ src/main/java/org/loja/model/credencial/Credencial.java
@@ -1,11 +1,8 @@
 package org.loja.model.credencial;
 
 import org.loja.model.Model;
-import java.util.List;
 import java.util.Set;
-import java.util.HashSet;
 import java.util.Date;
-//import org.loja.model.usuario.UsuarioCredencial;
 import org.loja.model.autorizacao.Autorizacao;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -29,12 +26,7 @@ public class Credencial extends Model {
 
   @OneToMany(fetch = FetchType.EAGER)
   @Fetch(FetchMode.SELECT)
-  private List<Autorizacao> autorizacoes;
-
-  /*@OneToMany(fetch = FetchType.EAGER, mappedBy = "usuario")
-  @Fetch(FetchMode.SELECT)
-  @JsonIgnore
-  private Set<UsuarioCredencial> usuario;*/
+  private Set<Autorizacao> autorizacoes;
 
   @Override
   public Integer getId() {
@@ -53,21 +45,13 @@ public class Credencial extends Model {
     this.nome = nome;
   }
 
-  public List<Autorizacao> getAutorizacoes() {
+  public Set<Autorizacao> getAutorizacoes() {
     return autorizacoes;
   }
 
-  public void setAutorizacoes(List<Autorizacao> autorizacoes) {
+  public void setAutorizacoes(Set<Autorizacao> autorizacoes) {
     this.autorizacoes = autorizacoes;
   }
-
-  /*public Set<UsuarioCredencial> getUsuario() {
-    return usuario;
-  }
-
-  public void setUsuario(Set<UsuarioCredencial> usuario) {
-    this.usuario = usuario;
-  }*/
 
   @Override
   public String toString() {
