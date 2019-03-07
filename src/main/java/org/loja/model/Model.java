51ebad2fd17bc3e465706101989a0b531f4b9a60
Kleber Mota <klebermo@gmail.com>
2019-03-07 12:09:08 -0300

    Model.java

--- src/main/java/org/loja/model/Model.java
+++ src/main/java/org/loja/model/Model.java
@@ -3,7 +3,10 @@ package org.loja.model;
 public abstract class Model extends Object {
   public abstract Object getId();
 
-  public boolean equals(Model object) {
+  @Override
+  public boolean equals(Object object) {
+    System.out.println("self: "+this.toString());
+    System.out.println("other: "+object.toString());
     return toString().equals(object.toString());
   }
 
