2978c0b405261d110d03d63aa194883466311f97
Kleber Mota <klebermo@gmail.com>
2019-03-19 10:50:05 -0300

    Model.java

--- src/main/java/org/loja/model/Model.java
+++ src/main/java/org/loja/model/Model.java
@@ -1,11 +1,22 @@
 package org.loja.model;
 
 public abstract class Model extends Object {
-  public abstract Object getId();
+  public abstract Integer getId();
 
   @Override
   public boolean equals(Object object) {
-    return toString().equals(object.toString());
+    Model other = (Model)object;
+    System.out.println("self: "+toString());
+    System.out.println("other: "+other.toString());
+    return toString().equals(other.toString());
+  }
+
+  @Override
+  public int hashCode() {
+      int hash = 7;
+      hash = 31 * hash + (int) getId();
+      hash = 31 * hash + (toString() == null ? 0 : toString().hashCode());
+      return hash;
   }
 
   public abstract String toString();
