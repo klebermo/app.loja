72cbb57868a0a62e08bbf5cf5ed26f35749c3b3b
Kleber Mota <klebermo@gmail.com>
2019-04-02 10:46:29 -0300

    model Autorizacao

--- src/main/java/org/loja/model/autorizacao/AutorizacaoEditor.java
+++ src/main/java/org/loja/model/autorizacao/AutorizacaoEditor.java
@@ -11,9 +11,12 @@ public class AutorizacaoEditor extends PropertyEditorSupport {
 
   @Override
   public void setAsText(String text) {
+    System.out.println("text='"+text+"'");
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      System.out.println("number='"+id+"'");
       Autorizacao autorizacao = serv.findBy("id", id);
+      System.out.println("autorizacao='"+autorizacao+"'");
       setValue(autorizacao);
     } else {
       setValue(null);
