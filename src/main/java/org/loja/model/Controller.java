c005005cd98266f1fc5239056b5f9318815176b3
Kleber Mota de Oliveira <klebermo@gmail.com>
2019-09-06 13:29:50 -0300

    Controller.java

--- src/main/java/org/loja/model/Controller.java
+++ src/main/java/org/loja/model/Controller.java
@@ -98,6 +98,7 @@ public abstract class Controller<E> {
     binder.registerCustomEditor(org.loja.model.cesta.Cesta.class, new org.loja.model.cesta.CestaEditor());
     binder.registerCustomEditor(org.loja.model.credencial.Credencial.class, new org.loja.model.credencial.CredencialEditor());
     binder.registerCustomEditor(org.loja.model.imagem.Imagem.class, new org.loja.model.imagem.ImagemEditor());
+    binder.registerCustomEditor(org.loja.model.pagina.Pagina.class, new org.loja.model.pagina.PaginaEditor());
     binder.registerCustomEditor(org.loja.model.pedido.Pedido.class, new org.loja.model.pedido.PedidoEditor());
     binder.registerCustomEditor(org.loja.model.produto.Produto.class, new org.loja.model.produto.ProdutoEditor());
     binder.registerCustomEditor(org.loja.model.usuario.Usuario.class, new org.loja.model.usuario.UsuarioEditor());
