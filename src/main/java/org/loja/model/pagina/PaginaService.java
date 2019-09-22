9a18bf9a041c9d2b3700b855870f8630c5c50be5
Kleber Mota <klebermo@gmail.com>
2019-09-22 09:25:28 -0300

    model Pagina

--- src/main/java/org/loja/model/pagina/PaginaService.java
+++ src/main/java/org/loja/model/pagina/PaginaService.java
@@ -1,10 +1,22 @@
 package org.loja.model.pagina;
 
 import org.springframework.stereotype.Service;
+import org.loja.model.pagina.PageTree.Node;
+import org.loja.model.pagina.PageTree.Tree;
+import java.util.List;
+import java.util.Collections;
 
 @Service
 public class PaginaService extends org.loja.model.Service<Pagina> {
   public PaginaService() {
     super(Pagina.class);
   }
+
+  public List<Node> breadcrumb(Pagina pagina) {
+  	Tree tree = new Tree(this.select());
+  	List<Node> lista = tree.search(pagina);
+  	Collections.reverse(lista);
+  	lista.remove(lista.size()-1);
+  	return lista;
+	}
 }
