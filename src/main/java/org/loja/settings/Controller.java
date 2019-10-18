aa49ba2a432571b5d1548a7ac1c817f5d4e185ee
Kleber Oliveira <klebermo@gmail.com>
2019-10-18 15:52:16 -0300

    settings generic classes

--- src/main/java/org/loja/settings/Controller.java
+++ src/main/java/org/loja/settings/Controller.java
@@ -5,18 +5,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public abstract class Controller<E> {
   @Autowired
-  private Service<E> serv;
+  protected Service<E> serv;
 
   private Class<E> clazz;
 
   public Controller(Class<E> clazz) {
     this.clazz = clazz;
+  }
+
+  @RequestMapping(value = "/checkout", method=RequestMethod.POST)
+  public String checkout(@RequestParam("usuario_id") Integer usuario_id) {
+    return "redirect:"+this.serv.checkout(usuario_id);
+  }
+
+  @RequestMapping(value = "/process_order", method=RequestMethod.POST)
+  public String process_order(@RequestParam("usuario_id") Integer usuario_id, @RequestParam("transaction_id") String transaction_id) {
+    return "redirect:"+this.serv.create_order(usuario_id, transaction_id);
   }
 
   @RequestMapping(value = "/get")
--- src/main/java/org/loja/settings/Service.java
+++ src/main/java/org/loja/settings/Service.java
@@ -1,15 +1,80 @@
 package org.loja.settings;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.loja.model.usuario.UsuarioDao;
+import org.loja.model.usuario.Usuario;
+import org.loja.model.cesta.CestaDao;
+import org.loja.model.cesta.Cesta;
+import org.loja.model.pedido.PedidoDao;
+import org.loja.model.pedido.Pedido;
+import org.loja.model.produto.ProdutoDao;
+import org.loja.model.produto.Produto;
+import java.util.ArrayList;
 
 public abstract class Service<E> {
   @Autowired
-  private Dao<E> dao;
+  protected Dao<E> dao;
+
+  @Autowired
+  protected UsuarioDao usuarioDao;
+
+  @Autowired
+  protected CestaDao cestaDao;
+
+  @Autowired
+  protected ProdutoDao produtoDao;
+
+  @Autowired
+  protected PedidoDao pedidoDao;
 
   private Class<E> clazz;
 
   public Service(Class<E> clazz) {
     this.clazz = clazz;
+  }
+
+  public String checkout(Integer usuario_id) {
+    Usuario usuario = usuarioDao.findBy("id", usuario_id);
+    String transaction_id = transaction_id();
+    return create_order(usuario_id, transaction_id);
+  }
+
+  public String create_order(Integer usuario_id, String transaction_id) {
+    Usuario usuario = usuarioDao.findBy("id", usuario_id);
+    return "/order/" + create_order(usuario, clazz.getSimpleName(), transaction_id);
+  }
+
+  public Integer create_order(Usuario usuario, String metogoPagamento, String transaction_id) {
+    Pedido pedido = new Pedido();
+    pedido.setProdutos(new ArrayList<Produto>());
+
+    Cesta cesta = usuario.getCesta();
+    if(cesta.getProdutos() != null) {
+      for(Produto produto : cesta.getProdutos())
+        pedido.getProdutos().add(produto);
+      cesta.getProdutos().clear();
+      cestaDao.update(cesta);
+    }
+
+    pedido.setDataCompra(new java.util.Date());
+    pedido.setMetogoPagamento(metogoPagamento);
+    pedido.setTransactionId(transaction_id);
+    pedidoDao.insert(pedido);
+
+    if(usuario.getPedidos() == null) {
+      usuario.setPedidos(new ArrayList<Pedido>());
+      usuario.getPedidos().add(pedido);
+      usuarioDao.update(usuario);
+    } else {
+      usuario.getPedidos().add(pedido);
+      usuarioDao.update(usuario);
+    }
+
+    return pedido.getId();
+  }
+
+  public String transaction_id() {
+    return "";
   }
 
   public E get() {
