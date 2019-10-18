b327cac08e42975106fcd60003e862d75a0ce196
Kleber Oliveira <klebermo@gmail.com>
2019-10-18 15:51:52 -0300

    setting PagSeguro

--- src/main/java/org/loja/settings/pagseguro/PagSeguroService.java
+++ src/main/java/org/loja/settings/pagseguro/PagSeguroService.java
@@ -1,10 +1,50 @@
 package org.loja.settings.pagseguro;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.uol.pagseguro.api.PagSeguroEnv;
+import br.com.uol.pagseguro.api.checkout.CheckoutRegistrationBuilder;
+import br.com.uol.pagseguro.api.checkout.RegisteredCheckout;
+import br.com.uol.pagseguro.api.common.domain.builder.PaymentItemBuilder;
+import br.com.uol.pagseguro.api.credential.Credential;
+import br.com.uol.pagseguro.api.common.domain.enums.Currency;
+import br.com.uol.pagseguro.api.http.JSEHttpClient;
+import br.com.uol.pagseguro.api.utils.logging.SimpleLoggerFactory;
+
+import org.loja.model.usuario.UsuarioDao;
+import org.loja.model.usuario.Usuario;
+import org.loja.model.produto.Produto;
+
+import java.math.BigDecimal;
 
 @Service
 public class PagSeguroService extends org.loja.settings.Service<PagSeguro> {
   public PagSeguroService() {
     super(PagSeguro.class);
+  }
+
+  public String checkout(Integer usuario_id) {
+    Usuario usuario = usuarioDao.findBy("id", usuario_id);
+
+    String email = ((PagSeguro) this.dao.get()).getEmail();
+    String token = ((PagSeguro) this.dao.get()).getToken();
+
+    final br.com.uol.pagseguro.api.PagSeguro pagSeguro = br.com.uol.pagseguro.api.PagSeguro.instance(new SimpleLoggerFactory(), new JSEHttpClient(), Credential.sellerCredential(email, token), PagSeguroEnv.SANDBOX);
+
+    CheckoutRegistrationBuilder registrationBuilder = new CheckoutRegistrationBuilder();
+    registrationBuilder.withCurrency(Currency.BRL);
+    for(Produto p : usuario.getCesta().getProdutos()) {
+      PaymentItemBuilder item = new PaymentItemBuilder();
+      item.withId(p.getId().toString());
+      item.withDescription(p.getNome().toString());
+      item.withAmount(new BigDecimal(p.getPreco()));
+      item.withQuantity(1);
+      registrationBuilder.addItem(item);
+    }
+
+    RegisteredCheckout registeredCheckout = pagSeguro.checkouts().register(registrationBuilder);
+
+    return registeredCheckout.getRedirectURL();
   }
 }
