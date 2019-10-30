185cdeb9c41965cac6adb08b69b112ee95a2041c
Kleber Oliveira <klebermo@gmail.com>
2019-10-30 18:08:50 -0300

    forum Forum

--- src/main/java/org/loja/forum/forum/ForumController.java
+++ src/main/java/org/loja/forum/forum/ForumController.java
@@ -2,11 +2,19 @@ package org.loja.forum.forum;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-@RequestMapping("forum")
-public class ForumController extends org.loja.forum.Service<Forum> {
+public class ForumController extends org.loja.forum.Controller<Forum> {
   public ForumController() {
     super(Forum.class);
+  }
+
+  @Override
+  @RequestMapping("/view_forum/{item_id}")
+  public String view(Model model, @PathVariable("item_id") Integer item_id) {
+    model.addAttribute("forum", this.serv.getData(item_id));
+    return "index";
   }
 }
