5c11e5e1c192c805d858cce2d40e2e4a86abe777
Kleber Oliveira <klebermo@gmail.com>
2019-10-30 17:12:32 -0300

    Service.java

---
+++ src/main/java/org/loja/forum/Service.java
@@ -0,0 +1,22 @@
+package org.loja.forum;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class Service<E extends org.loja.model.Model> {
+  private Class<E> clazz;
+
+  @Autowired
+  protected org.loja.model.Dao<E> dao;
+
+  public Service(Class<E> clazz) {
+    this.clazz = clazz;
+  }
+
+  public Class<E> getClazz() {
+    return this.clazz;
+  }
+
+  public void setClazz(Class<E> clazz) {
+    this.clazz = clazz;
+  }
+}
