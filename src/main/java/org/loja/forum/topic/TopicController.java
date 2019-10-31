a620e27d2826290aa781690fbdad3c6ad0e495e9
Kleber Mota <klebermo@gmail.com>
2019-10-31 08:37:05 -0300

    forum Topic

--- src/main/java/org/loja/forum/topic/TopicController.java
+++ src/main/java/org/loja/forum/topic/TopicController.java
@@ -2,19 +2,11 @@ package org.loja.forum.topic;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequestMapping("topic")
 public class TopicController extends org.loja.forum.Controller<Topic> {
   public TopicController() {
     super(Topic.class);
-  }
-
-  @Override
-  @RequestMapping("/view_topic/{item_id}")
-  public String view(Model model, @PathVariable("item_id") Integer item_id) {
-    model.addAttribute("topic", this.serv.getData(item_id));
-    return "index";
   }
 }
