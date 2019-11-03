09181cd37710ecb03a1dcd4a47455571fe9bd168
Kleber Mota <klebermo@gmail.com>
2019-11-03 13:59:42 -0300

    js scripts

--- src/main/resources/static/js/script-admin.js
+++ src/main/resources/static/js/script-admin.js
@@ -266,9 +266,9 @@ function submit_setting(element) {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var result = this.responseText;
       if(result === '')
-        document.getElementById('sucess').removeAttribute('style');
+        document.getElementById('ok').style.display = block;
       else
-        document.getElementById('error').removeAttribute('style');
+        document.getElementById('error').style.display = block;
     }
   };
   xhr.send(new FormData(form));
