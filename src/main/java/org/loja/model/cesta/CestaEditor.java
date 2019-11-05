20d9bd1a208202b87c29e0f400d9a7a32bc29732
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:50:42 -0300

    model Cesta

--- src/main/java/org/loja/model/cesta/CestaEditor.java
+++ src/main/java/org/loja/model/cesta/CestaEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.cesta;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class CestaEditor extends PropertyEditorSupport {
-  @Autowired
-  private CestaService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      CestaService serv = new CestaService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Cesta cesta = serv.findBy("id", id);
       setValue(cesta);
     } else {
