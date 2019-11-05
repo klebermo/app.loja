cf0d2b72798c7bd54a857f361206d4381a9eef52
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:52:09 -0300

    model Texto

--- src/main/java/org/loja/model/texto/TextoEditor.java
+++ src/main/java/org/loja/model/texto/TextoEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.texto;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class TextoEditor extends PropertyEditorSupport {
-  @Autowired
-  private TextoService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      TextoService serv = new TextoService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Texto texto = serv.findBy("id", id);
       setValue(texto);
     } else {
