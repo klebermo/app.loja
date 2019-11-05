fb64443f51fc425fd4f12dfa5081adc408d6bcd6
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:51:35 -0300

    model Produto

--- src/main/java/org/loja/model/produto/ProdutoEditor.java
+++ src/main/java/org/loja/model/produto/ProdutoEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.produto;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class ProdutoEditor extends PropertyEditorSupport {
-  @Autowired
-  private ProdutoService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      ProdutoService serv = new ProdutoService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Produto produto = serv.findBy("id", id);
       setValue(produto);
     } else {
