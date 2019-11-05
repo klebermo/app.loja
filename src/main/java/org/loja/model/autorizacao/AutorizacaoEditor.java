ee07f18fdc7e9ab3ce1b68d99de518e6825c45f4
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:50:10 -0300

    model Autorizacao

--- src/main/java/org/loja/model/autorizacao/AutorizacaoEditor.java
+++ src/main/java/org/loja/model/autorizacao/AutorizacaoEditor.java
@@ -1,22 +1,15 @@
 package org.loja.model.autorizacao;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class AutorizacaoEditor extends PropertyEditorSupport {
-  @Autowired
-  private AutorizacaoService serv;
-
   @Override
   public void setAsText(String text) {
-    System.out.println("text='"+text+"'");
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
-      System.out.println("number='"+id+"'");
+      AutorizacaoService serv = new AutorizacaoService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Autorizacao autorizacao = serv.findBy("id", id);
-      System.out.println("autorizacao='"+autorizacao+"'");
       setValue(autorizacao);
     } else {
       setValue(null);
