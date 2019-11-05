a3d11453703294cf258cbd9386fe16e9e20c4483
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:50:27 -0300

    model Categoria

--- src/main/java/org/loja/model/categoria/CategoriaEditor.java
+++ src/main/java/org/loja/model/categoria/CategoriaEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.categoria;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class CategoriaEditor extends PropertyEditorSupport {
-  @Autowired
-  private CategoriaService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      CategoriaService serv = new CategoriaService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Categoria categoria = serv.findBy("id", id);
       setValue(categoria);
     } else {
