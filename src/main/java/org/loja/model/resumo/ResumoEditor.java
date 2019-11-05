88c0041605952ccfc24a7571590733d429d8ca88
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:51:53 -0300

    model Resumo

--- src/main/java/org/loja/model/resumo/ResumoEditor.java
+++ src/main/java/org/loja/model/resumo/ResumoEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.resumo;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class ResumoEditor extends PropertyEditorSupport {
-  @Autowired
-  private ResumoService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      ResumoService serv = new ResumoService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Resumo resumo = serv.findBy("id", id);
       setValue(resumo);
     } else {
