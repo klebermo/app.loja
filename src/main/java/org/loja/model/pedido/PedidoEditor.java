64c7316d9fc5b522181729cfc7304a4027208155
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:51:20 -0300

    model Pedido

--- src/main/java/org/loja/model/pedido/PedidoEditor.java
+++ src/main/java/org/loja/model/pedido/PedidoEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.pedido;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class PedidoEditor extends PropertyEditorSupport {
-  @Autowired
-  private PedidoService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      PedidoService serv = new PedidoService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Pedido pedido = serv.findBy("id", id);
       setValue(pedido);
     } else {
