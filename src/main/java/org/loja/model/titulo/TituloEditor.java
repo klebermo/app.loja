6a131b409a7f4b19cc7e1244928d19cb715e6bf4
Kleber Mota <klebermo@gmail.com>
2019-11-05 09:52:34 -0300

    model Titulo

--- src/main/java/org/loja/model/titulo/TituloEditor.java
+++ src/main/java/org/loja/model/titulo/TituloEditor.java
@@ -1,18 +1,14 @@
 package org.loja.model.titulo;
 
-import org.springframework.stereotype.Component;
 import java.beans.PropertyEditorSupport;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
 public class TituloEditor extends PropertyEditorSupport {
-  @Autowired
-  private TituloService serv;
-
   @Override
   public void setAsText(String text) {
     if (!text.equals("")) {
       Integer id = Integer.parseInt(text);
+      TituloService serv = new TituloService();
+      org.loja.AppContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(serv);
       Titulo titulo = serv.findBy("id", id);
       setValue(titulo);
     } else {
