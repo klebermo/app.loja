9587c1d5de3e298088b93a4281447b5b6a9d6185
Kleber Mota <klebermo@gmail.com>
2019-11-09 09:33:07 -0300

    Service.java

--- src/main/java/org/loja/model/Service.java
+++ src/main/java/org/loja/model/Service.java
@@ -2,8 +2,21 @@ package org.loja.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import org.springframework.mail.javamail.JavaMailSender;
+import javax.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.context.Context;
+import java.util.Locale;
+import javax.mail.MessagingException;
+import org.thymeleaf.TemplateEngine;
 
 public abstract class Service<E> {
+  @Autowired
+  private TemplateEngine templateEngine;
+
+  @Autowired
+  private JavaMailSender mailSender;
+
   private Class<E> clazz;
 
   @Autowired
@@ -35,5 +48,25 @@ public abstract class Service<E> {
 
   public E newObject() throws InstantiationException, IllegalAccessException {
     return clazz.newInstance();
+  }
+
+  public void send_mail(String from, String nome, String sobrenome, String subject, String content, Locale locale) throws MessagingException {
+      // Prepare the evaluation context
+      Context ctx = new Context(locale);
+      ctx.setVariable("name", nome + " " + sobrenome);
+
+      // Prepare message using a Spring helper
+      MimeMessage mimeMessage = this.mailSender.createMimeMessage();
+      MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8"); // true = multipart
+      message.setSubject(subject);
+      message.setFrom("sender@mail.com");
+      message.setTo(from);
+
+      // Create the HTML body using Thymeleaf
+      String htmlContent = this.templateEngine.process(content, ctx);
+      message.setText(htmlContent, true); // true = isHtml
+
+      // Send mail
+      this.mailSender.send(mimeMessage);
   }
 }
