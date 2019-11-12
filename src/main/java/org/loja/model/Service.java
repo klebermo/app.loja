a3506a704e3cdf266fe1a43f966f5879e24b08d1
Kleber Mota <klebermo@gmail.com>
2019-11-11 21:07:48 -0300

    Service.java

--- src/main/java/org/loja/model/Service.java
+++ src/main/java/org/loja/model/Service.java
@@ -2,6 +2,7 @@ package org.loja.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.mail.javamail.JavaMailSender;
 import javax.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -11,6 +12,9 @@ import javax.mail.MessagingException;
 import org.thymeleaf.TemplateEngine;
 
 public abstract class Service<E> {
+  @Autowired
+  private HttpServletRequest httpServletRequest;
+
   @Autowired
   private TemplateEngine templateEngine;
 
@@ -50,8 +54,9 @@ public abstract class Service<E> {
     return clazz.newInstance();
   }
 
-  public void send_mail(String from, String nome, String sobrenome, String subject, String content, Locale locale) throws MessagingException {
+  public void send_mail(String from, String nome, String sobrenome, String subject, String content) throws MessagingException {
       // Prepare the evaluation context
+      Locale locale = httpServletRequest.getLocale();
       Context ctx = new Context(locale);
       ctx.setVariable("name", nome + " " + sobrenome);
 
