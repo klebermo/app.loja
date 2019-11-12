42ee0231766b606ae2df641d28c9a6a316de3de8
Kleber Mota <klebermo@gmail.com>
2019-11-12 11:04:55 -0300

    Service.java

--- src/main/java/org/loja/model/Service.java
+++ src/main/java/org/loja/model/Service.java
@@ -2,26 +2,9 @@ package org.loja.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.mail.javamail.JavaMailSender;
-import javax.mail.internet.MimeMessage;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.thymeleaf.context.Context;
-import java.util.Locale;
-import javax.mail.MessagingException;
-import org.thymeleaf.TemplateEngine;
 
 public abstract class Service<E> {
-  @Autowired
-  private HttpServletRequest httpServletRequest;
-
-  @Autowired
-  private TemplateEngine templateEngine;
-
-  @Autowired
-  private JavaMailSender mailSender;
-
-  private Class<E> clazz;
+  protected Class<E> clazz;
 
   @Autowired
   protected Dao<E> dao;
@@ -52,26 +35,5 @@ public abstract class Service<E> {
 
   public E newObject() throws InstantiationException, IllegalAccessException {
     return clazz.newInstance();
-  }
-
-  public void send_mail(String from, String nome, String sobrenome, String subject, String content) throws MessagingException {
-      // Prepare the evaluation context
-      Locale locale = httpServletRequest.getLocale();
-      Context ctx = new Context(locale);
-      ctx.setVariable("name", nome + " " + sobrenome);
-
-      // Prepare message using a Spring helper
-      MimeMessage mimeMessage = this.mailSender.createMimeMessage();
-      MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8"); // true = multipart
-      message.setSubject(subject);
-      message.setFrom("sender@mail.com");
-      message.setTo(from);
-
-      // Create the HTML body using Thymeleaf
-      String htmlContent = this.templateEngine.process(content, ctx);
-      message.setText(htmlContent, true); // true = isHtml
-
-      // Send mail
-      this.mailSender.send(mimeMessage);
   }
 }
