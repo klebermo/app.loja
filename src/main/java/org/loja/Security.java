c1754845326e3cdf0b5ba3e644c0bf4f59bcac63
Kleber Mota <klebermo@gmail.com>
2019-11-18 10:32:44 -0300

    Security.java

--- src/main/java/org/loja/Security.java
+++ src/main/java/org/loja/Security.java
@@ -26,7 +26,7 @@ public class Security extends WebSecurityConfigurerAdapter {
         .jdbcAuthentication()
           .dataSource(dataSource)
           .usersByUsernameQuery("select username, password, enabled from usuario where username = ?")
-          .authoritiesByUsernameQuery("select t1.username, t2.authority from (select u.username as username, c.nome as credencial from usuario u, usuario_credenciais uc, credencial c where u.id = uc.usuario_id and c.id = uc.credenciais_id) t1 inner join (select c.nome as credencial, a.nome as authority from credencial c, credencial_autorizacoes ca, autorizacao a where c.id = ca.credencial_id and a.id = ca.autorizacoes_id) t2 on t1.credencial = t2.credencial where t1.username =  ?")
+          .authoritiesByUsernameQuery("select t1.username, t2.authority from (select u.username as username, c.nome as credencial from usuario u, usuario_credenciais uc, credencial c where u.id = uc.usuario_id and c.id = uc.credencial_id) t1 inner join (select c.nome as credencial, a.nome as authority from credencial c, credencial_autorizacoes ca, autorizacao a where c.id = ca.credencial_id and a.id = ca.autorizacoes_id) t2 on t1.credencial = t2.credencial where t1.username =  ?")
           .passwordEncoder(new BCryptPasswordEncoder(4));
   }
 
