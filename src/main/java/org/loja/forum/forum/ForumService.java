f66d571229b3a027490c779d7e54cbb30b11932e
Kleber Mota <klebermo@gmail.com>
2019-11-29 11:07:13 -0300

    forum Forum

--- src/main/java/org/loja/forum/forum/ForumService.java
+++ src/main/java/org/loja/forum/forum/ForumService.java
@@ -1,10 +1,20 @@
 package org.loja.forum.forum;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.loja.model.produto.ProdutoDao;
+import org.loja.model.produto.Produto;
 
 @Service
 public class ForumService extends org.loja.forum.Service<Forum> {
+  @Autowired
+  private ProdutoDao produtoDao;
+
   public ForumService() {
     super(Forum.class);
+  }
+
+  public Produto forum_owner(Integer forum_id) {
+    return produtoDao.findBy("forum_id", forum_id);
   }
 }
