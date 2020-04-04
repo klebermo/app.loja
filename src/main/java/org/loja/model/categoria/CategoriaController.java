6d0d0870e09d723c1453191a799b0080885f436e
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-04 11:29:06 -0300

    model Categoria

--- src/main/java/org/loja/model/categoria/CategoriaController.java
+++ src/main/java/org/loja/model/categoria/CategoriaController.java
@@ -2,11 +2,23 @@ package org.loja.model.categoria;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("categoria")
 public class CategoriaController extends org.loja.model.Controller<Categoria> {
   public CategoriaController() {
     super(Categoria.class);
+  }
+
+  @Autowired
+  private CategoriaService serv;
+
+  @RequestMapping("/{nome}")
+  public String categoria(Model model, @PathVariable("nome") String nome) {
+    model.addAttribute("categoria", this.serv.categoria(nome));
+    return "index";
   }
 }
--- src/main/java/org/loja/model/categoria/CategoriaService.java
+++ src/main/java/org/loja/model/categoria/CategoriaService.java
@@ -1,10 +1,21 @@
 package org.loja.model.categoria;
 
 import org.springframework.stereotype.Service;
+import org.loja.model.titulo.Titulo;
 
 @Service
 public class CategoriaService extends org.loja.model.Service<Categoria> {
   public CategoriaService() {
     super(Categoria.class);
+  }
+
+  public Categoria categoria(String titulo) {
+    for(Categoria c : this.dao.select()) {
+      for(Titulo t : c.getNome()) {
+        if(t.getConteudo().equals(titulo))
+          return c;
+      }
+    }
+    return null;
   }
 }
