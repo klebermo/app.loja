da59727c871f8d5fba9131a310b7264bda928dc8
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-04 11:27:35 -0300

    Update Controller.java

--- src/main/java/org/loja/forum/Controller.java
+++ src/main/java/org/loja/forum/Controller.java
@@ -2,36 +2,22 @@ package org.loja.forum;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.WebDataBinder;
-import org.loja.model.produto.ProdutoService;
-import org.loja.model.produto.Produto;
-import org.loja.model.usuario.UsuarioService;
-import org.loja.model.usuario.Usuario;
-import java.util.List;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
 
-public abstract class Controller<E extends org.loja.model.Model> {
-  private Class<E> clazz;
-
+public abstract class Controller<E extends org.loja.forum.Model> {
   @Autowired
   protected Service<E> serv;
 
-  @Autowired
-  private ProdutoService produtoService;
-
-  @Autowired
-  private UsuarioService usuarioService;
+  private Class<E> clazz;
 
   public Controller(Class<E> clazz) {
     this.clazz = clazz;
   }
 
-  @ModelAttribute("classe")
   public Class<E> getClazz() {
     return this.clazz;
   }
@@ -40,33 +26,21 @@ public abstract class Controller<E extends org.loja.model.Model> {
     this.clazz = clazz;
   }
 
-  @ModelAttribute("target")
-  public String getName() {
-    return clazz.getSimpleName().toLowerCase();
-  }
-
-  @RequestMapping("/view/{item_id}")
-  public String view(Model model, @PathVariable("item_id") Integer item_id, @RequestParam(value="page", required=false, defaultValue="1") Integer page) {
-    model.addAttribute(this.getName(), this.serv.getData(item_id));
-    model.addAttribute("page", page);
+  @RequestMapping(value="/view", method=RequestMethod.GET)
+  public String view(Model model, @RequestParam("item") Integer id) {
+    model.addAttribute(getClazz().getSimpleName().toLowerCase(), this.serv.get(id));
     return "index";
   }
 
-  @ModelAttribute("produtos")
-  public List<Produto> produtos() {
-    return produtoService.select();
+  @RequestMapping(value="/save", method=RequestMethod.POST)
+  public void save(@Valid E object, BindingResult result) {
+    this.serv.set(object);
   }
 
-  @ModelAttribute("usuario")
-  public Usuario usuario() {
-    return usuarioService.findBy("username", SecurityContextHolder.getContext().getAuthentication().getName());
-  }
-
-  @InitBinder
-  public void initBinder(WebDataBinder binder) {
-    binder.registerCustomEditor(java.util.Date.class, new org.loja.model.usuario.DateEditor());
-    binder.registerCustomEditor(org.loja.forum.forum.Forum.class, new org.loja.forum.forum.ForumEditor());
-    binder.registerCustomEditor(org.loja.forum.resposta.Resposta.class, new org.loja.forum.resposta.RespostaEditor());
-    binder.registerCustomEditor(org.loja.forum.topic.Topic.class, new org.loja.forum.topic.TopicEditor());
+  @RequestMapping(value="/new", method=RequestMethod.GET)
+  public String newObject(Model model, @RequestParam("item") Integer id) throws InstantiationException, IllegalAccessException {
+    model.addAttribute(getClazz().getSimpleName().toLowerCase(), this.serv.get(id));
+    model.addAttribute("command", this.serv.newObject());
+    return "index";
   }
 }
