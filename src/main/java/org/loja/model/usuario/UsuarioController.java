0e88023a8f74359a71e693390c650171643fc64f
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-04 11:29:27 -0300

    model Usuario

--- src/main/java/org/loja/model/usuario/UsuarioController.java
+++ src/main/java/org/loja/model/usuario/UsuarioController.java
@@ -1,12 +1,47 @@
 package org.loja.model.usuario;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("usuario")
 public class UsuarioController extends org.loja.model.Controller<Usuario> {
   public UsuarioController() {
     super(Usuario.class);
+  }
+
+  @Autowired
+  private UsuarioService serv;
+
+  @RequestMapping(value = "/register", method=RequestMethod.GET)
+  public String formRegister(Model model) {
+    model.addAttribute("command", new Usuario());
+    return "register";
+  }
+
+  @RequestMapping(value = "/register", method=RequestMethod.POST)
+  @ResponseBody
+  public void doRegister(@ModelAttribute("command") Usuario object) throws Exception {
+    this.serv.register(object);
+  }
+
+  @RequestMapping(value = "/credenciais", method=RequestMethod.GET)
+  @PreAuthorize("hasPermission(#user, 'consulta_credencial'")
+  public String formCredenciais(Model model, @RequestParam("id") Integer id) {
+    model.addAttribute("command", this.serv.findBy("id", id));
+    return "admin/form/credenciais";
+  }
+
+  @RequestMapping(value = "/toggle_credencial", method=RequestMethod.POST)
+  @ResponseBody
+  public void toggle_credencial(@RequestParam("usuario_id") Integer usuario_id, @RequestParam("credencial_id") Integer credencial_id) {
+    this.serv.toggle_credencial(usuario_id, credencial_id);
   }
 }
--- src/main/java/org/loja/model/usuario/UsuarioService.java
+++ src/main/java/org/loja/model/usuario/UsuarioService.java
@@ -1,10 +1,47 @@
 package org.loja.model.usuario;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.loja.model.credencial.Credencial;
+import org.loja.model.credencial.CredencialDao;
+import java.util.ArrayList;
 
 @Service
 public class UsuarioService extends org.loja.model.Service<Usuario> {
   public UsuarioService() {
     super(Usuario.class);
+  }
+
+  @Autowired
+  private UsuarioDao usuarioDao;
+
+  @Autowired
+  private CredencialDao credencialDao;
+
+  public void register(Usuario novo) throws Exception {
+    novo.setEnabled(true);
+    novo.setLocked(false);
+    novo.setCredenciais(new ArrayList<Credencial>());
+    Credencial credencial = credencialDao.findBy("nome", "web");
+    novo.getCredenciais().add(credencial);
+    usuarioDao.insert(novo);
+  }
+
+  public void toggle_credencial(Integer usuario_id, Integer credencial_id) {
+    Usuario usuario = usuarioDao.findBy("id", usuario_id);
+    Credencial credencial = credencialDao.findBy("id", credencial_id);
+    if(usuario.getCredenciais() == null) {
+      usuario.setCredenciais(new ArrayList<Credencial>());
+      usuario.getCredenciais().add(credencial);
+      usuarioDao.update(usuario);
+    } else {
+      if(usuario.getCredenciais().contains(credencial)) {
+        usuario.getCredenciais().remove(credencial);
+        usuarioDao.update(usuario);
+      } else {
+        usuario.getCredenciais().add(credencial);
+        usuarioDao.update(usuario);
+      }
+    }
   }
 }
