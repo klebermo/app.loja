4416a16490295cea22decd1ba121defdc0b64aad
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-04 11:29:37 -0300

    Update ProdutoController.java

--- src/main/java/org/loja/model/produto/ProdutoController.java
+++ src/main/java/org/loja/model/produto/ProdutoController.java
@@ -2,11 +2,19 @@ package org.loja.model.produto;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("produto")
 public class ProdutoController extends org.loja.model.Controller<Produto> {
   public ProdutoController() {
     super(Produto.class);
+  }
+
+  @RequestMapping("/{nome}")
+  public String produto(Model model, @PathVariable("nome") String nome) {
+    model.addAttribute("produto", this.serv.findBy("nome", nome));
+    return "index";
   }
 }
