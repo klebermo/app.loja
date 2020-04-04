29b376ecdd36f4bcd8ae2b913537cea100ddc4f1
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-04 11:30:00 -0300

    Create CestaController.java

---
+++ src/main/java/org/loja/model/cesta/CestaController.java
@@ -0,0 +1,19 @@
+package org.loja.model.cesta;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("cart")
+public class CestaController extends org.loja.model.Controller<Cesta> {
+  public CestaController() {
+    super(Cesta.class);
+  }
+
+  @RequestMapping("/")
+  public String cesta(Model model) {
+    model.addAttribute("cart", "cart");
+    return "index";
+  }
+}
