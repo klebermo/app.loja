9ed6034a2d17ec1814b03ce9cb18bf97b54fa995
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-04 11:29:53 -0300

    model Pagina

--- src/main/java/org/loja/model/pagina/PaginaController.java
+++ src/main/java/org/loja/model/pagina/PaginaController.java
@@ -2,11 +2,25 @@ package org.loja.model.pagina;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("pagina")
 public class PaginaController extends org.loja.model.Controller<Pagina> {
   public PaginaController() {
     super(Pagina.class);
+  }
+
+  @Autowired
+  private PaginaService serv;
+
+  @RequestMapping("/{titulo}")
+  public String pagina(Model model, @PathVariable("titulo") String titulo) {
+    Pagina pagina = this.serv.pagina(titulo);
+    model.addAttribute("pagina", pagina);
+    model.addAttribute("breadcrumb", this.serv.breadcrumb(pagina));
+    return "index";
   }
 }
--- src/main/java/org/loja/model/pagina/PaginaService.java
+++ src/main/java/org/loja/model/pagina/PaginaService.java
@@ -5,11 +5,22 @@ import org.loja.model.pagina.PageTree.Node;
 import org.loja.model.pagina.PageTree.Tree;
 import java.util.List;
 import java.util.Collections;
+import org.loja.model.titulo.Titulo;
 
 @Service
 public class PaginaService extends org.loja.model.Service<Pagina> {
   public PaginaService() {
     super(Pagina.class);
+  }
+
+  public Pagina pagina(String titulo) {
+    for(Pagina p : this.dao.select()) {
+      for(Titulo t : p.getTitulo()) {
+        if(t.getConteudo().equals(titulo))
+          return p;
+      }
+    }
+    return null;
   }
 
   public List<Node> breadcrumb(Pagina pagina) {
