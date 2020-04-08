cabcb3e22de74846b2e5aec3baf685d5d6d2b304
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-08 14:45:51 -0300

    model Topic

--- src/main/java/org/loja/model/topic/TopicController.java
+++ src/main/java/org/loja/model/topic/TopicController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.loja.model.resposta.Resposta;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 @RequestMapping("topic")
@@ -12,6 +16,9 @@ public class TopicController extends org.loja.model.Controller<Topic> {
   public TopicController() {
     super(Topic.class);
   }
+
+  @Autowired
+  private TopicService serv;
 
   @RequestMapping(value="/view", method=RequestMethod.GET)
   public String view(Model model, @RequestParam("id") Integer id) {
@@ -24,5 +31,11 @@ public class TopicController extends org.loja.model.Controller<Topic> {
     model.addAttribute("forum_id", forum_id);
     model.addAttribute("new_topic", this.serv.newObject());
     return "index";
+  }
+
+  @RequestMapping(value="/add_resposta", method=RequestMethod.POST)
+  @ResponseBody
+  public void add_resposta(@RequestParam("topic") Integer topic_id, @ModelAttribute("resposta") Resposta resposta) {
+    this.serv.add_resposta(topic_id, resposta);
   }
 }
--- src/main/java/org/loja/model/topic/TopicService.java
+++ src/main/java/org/loja/model/topic/TopicService.java
@@ -1,10 +1,23 @@
 package org.loja.model.topic;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.loja.model.resposta.Resposta;
+import org.loja.model.resposta.RespostaDao;
 
 @Service
 public class TopicService extends org.loja.model.Service<Topic> {
   public TopicService() {
     super(Topic.class);
+  }
+
+  @Autowired
+  private RespostaDao respostaDao;
+
+  public void add_resposta(Integer topic_id, Resposta resposta_data) {
+    Topic topic = this.dao.findBy("id", topic_id);
+    Resposta resposta = respostaDao.insert(resposta_data);
+    topic.setResposta(resposta);
+    this.dao.update(topic);
   }
 }
