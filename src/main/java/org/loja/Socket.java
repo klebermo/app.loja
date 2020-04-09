a3ed5ecfdbe60c102e79eb9dbf8f79cd7a7579a2
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-08 21:02:09 -0300

    Create Socket.java

---
+++ src/main/java/org/loja/Socket.java
@@ -0,0 +1,17 @@
+package org.loja;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.loja.model.registro.CheckRegistro;
+import org.loja.model.registro.MakeRegistro;
+
+@Configuration
+@EnableWebSocket
+public class Socket implements WebSocketConfigurer {
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new CheckRegistro(), "/check_registration");
+    registry.addHandler(new MakeRegistro(), "/register_product");
+	}
+}
