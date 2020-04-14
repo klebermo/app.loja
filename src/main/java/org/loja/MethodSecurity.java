b7e4d9e0f803b4d632dceef7676c0544c07f8e1a
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-14 17:27:01 -0300

    Update MethodSecurity.java

--- src/main/java/org/loja/MethodSecurity.java
+++ src/main/java/org/loja/MethodSecurity.java
@@ -1,4 +1,4 @@
-package org.store.security;
+package org.loja.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
