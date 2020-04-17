5c7cba3dbf4e3e7a17bec2df6d923a0b55afb40e
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-17 16:06:40 -0300

    Create Install.java

---
+++ src/main/java/org/loja/Install.java
@@ -0,0 +1,18 @@
+package org.loja;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Install {
+  public Boolean estaInstalado() {
+    return true;
+  }
+
+  public void criarBanco() {
+    //
+  }
+
+  public void criarTabelas() {
+    //
+  }
+}
