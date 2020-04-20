2948ef451a11e1ee7240f5309ff7bcd8fd59c179
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-19 21:50:44 -0300

    Update CheckRegistro.java

--- src/main/java/org/loja/model/registro/CheckRegistro.java
+++ src/main/java/org/loja/model/registro/CheckRegistro.java
@@ -11,18 +11,43 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
+import org.loja.model.cliente.Cliente;
+import org.loja.model.cliente.ClienteService;
+import org.loja.model.usuario.Usuario;
+import org.loja.model.usuario.UsuarioService;
+import org.loja.model.produto.Produto;
+import org.loja.model.produto.ProdutoService;
 
 @Component
 public class CheckRegistro extends TextWebSocketHandler {
   @Autowired
   private RegistroService registroServ;
 
+  @Autowired
+  private UsuarioService usuarioServ;
+
+  @Autowired
+  private ProdutoService produtoServ;
+
+  @Autowired
+  private ClienteService clienteServ;
+
 	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
-    String token = new String(message.asBytes());
-    Registro value = registroServ.findBy("token", token);
+    Registro value = new Gson().fromJson(message.getPayload(), Registro.class);
+
+    String email_usuario = value.getUsuario().getEmail();
+    Usuario usuario = usuarioServ.findBy("email", email_usuario);
+
+    String nome_produto = value.getProduto().getNome();
+    Produto produto = produtoServ.findBy("nome", nome_produto);
+
+    Cliente cliente = clienteServ.findBy("usuario", usuario);
+    if(cliente.produtosComprados().contains(produto))
+      value = registroServ.findBy("produto", produto);
+
     String result = new Gson().toJson(value);
 		session.sendMessage(new TextMessage(result));
 	}
