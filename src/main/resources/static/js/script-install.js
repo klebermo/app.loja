98c579dbe4d52481887631ab6d5bad99176bed2e
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-21 17:32:43 -0300

    Update script-install.js

--- src/main/resources/static/js/script-install.js
+++ src/main/resources/static/js/script-install.js
@@ -23,8 +23,6 @@ function showTab(n) {
 function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab");
-  // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
@@ -39,28 +37,6 @@ function nextPrev(n) {
   showTab(currentTab);
 }
 
-function validateForm() {
-  // This function deals with validation of the form fields
-  var x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false:
-      valid = false;
-    }
-  }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid; // return the valid status
-}
-
 function fixStepIndicator(n) {
   // This function removes the "active" class of all steps...
   var i, x = document.getElementsByClassName("step");
@@ -72,5 +48,18 @@ function fixStepIndicator(n) {
 }
 
 function submit_install() {
-  //
+  console.log('install');
+  var form = document.getElementById("form");
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", form.action, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log('go to index');
+      document.location.href =  form.dataset.destino;
+    }
+  };
+
+  console.log('send data');
+  xhr.send(new FormData(form));
 }
