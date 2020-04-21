f6eca83aef323b3ab6e6bec37fbcab97c1b890ed
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-20 22:05:06 -0300

    Update Arquivo.java

--- src/main/java/org/loja/model/arquivo/Arquivo.java
+++ src/main/java/org/loja/model/arquivo/Arquivo.java
@@ -22,32 +22,106 @@ public class Arquivo extends Model implements Serializable {
   @Column
   private String type;
 
-  @Override
-  public Integer getId() {
-    return id;
-  }
+  @Column
+  private Integer version;
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+	/**
+	* Default empty Arquivo constructor
+	*/
+	public Arquivo() {
+		super();
+    this.version = 1;
+	}
 
-  public String getFileName() {
-    return fileName;
-  }
+	/**
+	* Default Arquivo constructor
+	*/
+	public Arquivo(Integer id, String fileName, String type, Integer version) {
+		super();
+		this.id = id;
+		this.fileName = fileName;
+		this.type = type;
+		this.version = version;
+	}
 
-  public void setFileName(String fileName) {
-    this.fileName = fileName;
-  }
+	/**
+	* Returns value of serialVersionUID
+	* @return
+	*/
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
 
-  public String getType() {
-    return type;
-  }
+	/**
+	* Returns value of id
+	* @return
+	*/
+	public Integer getId() {
+		return id;
+	}
 
-  public void setType(String type) {
-    this.type = type;
-  }
+	/**
+	* Sets new value of id
+	* @param
+	*/
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  public String toString() {
-    return fileName;
-  }
+	/**
+	* Returns value of fileName
+	* @return
+	*/
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	* Sets new value of fileName
+	* @param
+	*/
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	* Returns value of type
+	* @return
+	*/
+	public String getType() {
+		return type;
+	}
+
+	/**
+	* Sets new value of type
+	* @param
+	*/
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	* Returns value of version
+	* @return
+	*/
+	public Integer getVersion() {
+		return version;
+	}
+
+	/**
+	* Sets new value of version
+	* @param
+	*/
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	/**
+	* Create string representation of Arquivo for printing
+	* @return
+	*/
+	@Override
+	public String toString() {
+		return "Arquivo [id=" + id + ", fileName=" + fileName + ", type=" + type + ", version=" + version + "]";
+	}
 }
