23d62113637d7cc43116a9ad0a6da1421b2327f6
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-20 22:05:35 -0300

    Create RegistroWrapper.java

---
+++ src/main/java/org/loja/model/registro/RegistroWrapper.java
@@ -0,0 +1,13 @@
+package org.loja.model.registro;
+
+public class RegistroWrapper {
+  private Registro registro;
+
+  public Registro getRegistro() {
+    return registro;
+  }
+
+  public void setRegistro(Registro registro) {
+    this.registro = registro;
+  }
+}
