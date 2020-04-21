1c78e9a6f0c2bb2b279911b6fea4cc16bd18edcd
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-20 22:05:28 -0300

    Update Socket.java

--- src/main/java/org/loja/Socket.java
+++ src/main/java/org/loja/Socket.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.loja.model.registro.CheckRegistro;
 import org.loja.model.registro.MakeRegistro;
+import org.loja.model.produto.CheckUpdate;
 
 @Configuration
 @EnableWebSocket
@@ -13,5 +14,6 @@ public class Socket implements WebSocketConfigurer {
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(new CheckRegistro(), "/check_registration").setAllowedOrigins("*");
     registry.addHandler(new MakeRegistro(), "/register_product").setAllowedOrigins("*");
+		registry.addHandler(new CheckUpdate(), "/check_update").setAllowedOrigins("*");
 	}
 }
