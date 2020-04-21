f87323a231316ceb4914d1a4675ff3cf58892a82
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-20 22:05:11 -0300

    Update Imagem.java

--- src/main/java/org/loja/model/imagem/Imagem.java
+++ src/main/java/org/loja/model/imagem/Imagem.java
@@ -22,32 +22,85 @@ public class Imagem extends Model implements Serializable {
   @Column
   private String type;
 
-  @Override
-  public Integer getId() {
-    return id;
-  }
+	/**
+	* Default empty Imagem constructor
+	*/
+	public Imagem() {
+		super();
+	}
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+	/**
+	* Default Imagem constructor
+	*/
+	public Imagem(Integer id, String fileName, String type) {
+		super();
+		this.id = id;
+		this.fileName = fileName;
+		this.type = type;
+	}
 
-  public String getFileName() {
-    return fileName;
-  }
+	/**
+	* Returns value of serialVersionUID
+	* @return
+	*/
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
 
-  public void setFileName(String fileName) {
-    this.fileName = fileName;
-  }
+	/**
+	* Returns value of id
+	* @return
+	*/
+	public Integer getId() {
+		return id;
+	}
 
-  public String getType() {
-    return type;
-  }
+	/**
+	* Sets new value of id
+	* @param
+	*/
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  public void setType(String type) {
-    this.type = type;
-  }
+	/**
+	* Returns value of fileName
+	* @return
+	*/
+	public String getFileName() {
+		return fileName;
+	}
 
-  public String toString() {
-    return fileName;
-  }
+	/**
+	* Sets new value of fileName
+	* @param
+	*/
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	* Returns value of type
+	* @return
+	*/
+	public String getType() {
+		return type;
+	}
+
+	/**
+	* Sets new value of type
+	* @param
+	*/
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	* Create string representation of Imagem for printing
+	* @return
+	*/
+	@Override
+	public String toString() {
+		return "Imagem [id=" + id + ", fileName=" + fileName + ", type=" + type + "]";
+	}
 }
