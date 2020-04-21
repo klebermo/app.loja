f74a0477ad51b7892c67510d9a31eb1d96c5a9c8
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-20 22:05:32 -0300

    Create ProdutoWrapper.java

---
+++ src/main/java/org/loja/model/produto/ProdutoWrapper.java
@@ -0,0 +1,13 @@
+package org.loja.model.produto;
+
+public class ProdutoWrapper {
+  private Produto produto;
+
+  public Produto getProduto() {
+    return produto;
+  }
+
+  public void setProduto(Produto produto) {
+    this.produto = produto;
+  }
+}
