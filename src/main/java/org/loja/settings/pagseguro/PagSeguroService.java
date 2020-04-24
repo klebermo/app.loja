18d74b691ba2a5dc616a3f4087058297d086614a
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-23 22:04:52 -0300

    Update PagSeguroService.java

--- src/main/java/org/loja/settings/pagseguro/PagSeguroService.java
+++ src/main/java/org/loja/settings/pagseguro/PagSeguroService.java
@@ -2,7 +2,6 @@ package org.loja.settings.pagseguro;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import br.com.uol.pagseguro.api.PagSeguroEnv;
 import br.com.uol.pagseguro.api.checkout.CheckoutRegistrationBuilder;
 import br.com.uol.pagseguro.api.checkout.RegisteredCheckout;
@@ -11,17 +10,29 @@ import br.com.uol.pagseguro.api.credential.Credential;
 import br.com.uol.pagseguro.api.common.domain.enums.Currency;
 import br.com.uol.pagseguro.api.http.JSEHttpClient;
 import br.com.uol.pagseguro.api.utils.logging.SimpleLoggerFactory;
-
-import org.loja.model.cliente.ClienteDao;
 import org.loja.model.cliente.Cliente;
+import org.loja.model.cliente.ClienteDao;
+import org.loja.model.cesta.Cesta;
+import org.loja.model.cesta.CestaDao;
+import org.loja.model.pedido.Pedido;
+import org.loja.model.pedido.PedidoDao;
 import org.loja.model.produto.Produto;
-
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 @Service
 public class PagSeguroService extends org.loja.settings.Service<PagSeguro> {
+  @Autowired
+  private ClienteDao clienteDao;
+
+  @Autowired
+  private CestaDao cestaDao;
+
+  @Autowired
+  private PedidoDao pedidoDao;
+
   private Map<String, String> map = new HashMap<String, String>();
 
   public PagSeguroService() {
@@ -56,6 +67,30 @@ public class PagSeguroService extends org.loja.settings.Service<PagSeguro> {
   public String create_order(String transaction_id) {
     Integer cliente_id = Integer.valueOf(map.get("cliente_id"));
     Cliente cliente = (Cliente) this.clienteDao.findBy("id", cliente_id);
-    return "/order/" + this.create_order(cliente, this.clazz.getSimpleName(), transaction_id);
+
+    Pedido pedido = new Pedido();
+    pedidoDao.insert(pedido);
+
+    pedido.setProdutos(new ArrayList<Produto>());
+    Cesta cesta = cliente.getCesta();
+    pedido.getProdutos().addAll(cesta.getProdutos());
+    cesta.getProdutos().clear();
+    cestaDao.update(cesta);
+
+    pedido.setDataCompra(new java.util.Date());
+    pedido.setMetodoPagamento(clazz.getSimpleName());
+    pedido.setTransactionId(transaction_id);
+    pedidoDao.update(pedido);
+
+    if(cliente.getPedidos() == null) {
+      cliente.setPedidos(new ArrayList<Pedido>());
+      cliente.getPedidos().add(pedido);
+      clienteDao.update(cliente);
+    } else {
+      cliente.getPedidos().add(pedido);
+      clienteDao.update(cliente);
+    }
+
+    return "/pedido/" + pedido.getId().toString();
   }
 }
