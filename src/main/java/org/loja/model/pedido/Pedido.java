d089072685f9fe45b16ac55c820fc069db3d98cc
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-23 22:03:57 -0300

    Update Pedido.java

--- src/main/java/org/loja/model/pedido/Pedido.java
+++ src/main/java/org/loja/model/pedido/Pedido.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
 import javax.persistence.GeneratedValue;
@@ -16,6 +17,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.FetchType;
 import javax.persistence.CascadeType;
 import javax.persistence.OrderColumn;
+import org.loja.model.registro.Registro;
 
 @Entity
 public class Pedido extends Model {
@@ -37,48 +39,132 @@ public class Pedido extends Model {
   @Column(name="transaction_id")
   private String transactionId;
 
-  @Override
-  public Integer getId() {
-    return id;
-  }
+  @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  private Registro registro;
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
 
-  public List<Produto> getProdutos() {
-    return produtos;
-  }
+	/**
+	* Default empty Pedido constructor
+	*/
+	public Pedido() {
+		super();
+	}
 
-  public void setProdutos(List<Produto> produtos) {
-    this.produtos = produtos;
-  }
+	/**
+	* Default Pedido constructor
+	*/
+	public Pedido(Integer id, List<Produto> produtos, Date dataCompra, String metodoPagamento, String transactionId, Registro registro) {
+		super();
+		this.id = id;
+		this.produtos = produtos;
+		this.dataCompra = dataCompra;
+		this.metodoPagamento = metodoPagamento;
+		this.transactionId = transactionId;
+		this.registro = registro;
+	}
 
-  public Date getDataCompra() {
-    return dataCompra;
-  }
+	/**
+	* Returns value of id
+	* @return
+	*/
+	public Integer getId() {
+		return id;
+	}
 
-  public void setDataCompra(Date dataCompra) {
-    this.dataCompra = dataCompra;
-  }
+	/**
+	* Sets new value of id
+	* @param
+	*/
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  public String getMetodoPagamento() {
-    return metodoPagamento;
-  }
+	/**
+	* Returns value of produtos
+	* @return
+	*/
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
 
-  public void setMetodoPagamento(String metodoPagamento) {
-    this.metodoPagamento = metodoPagamento;
-  }
+	/**
+	* Sets new value of produtos
+	* @param
+	*/
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
-  public String getTransactionId() {
-    return transactionId;
-  }
+	/**
+	* Returns value of dataCompra
+	* @return
+	*/
+	public Date getDataCompra() {
+		return dataCompra;
+	}
 
-  public void setTransactionId(String transactionId) {
-    this.transactionId = transactionId;
-  }
+	/**
+	* Sets new value of dataCompra
+	* @param
+	*/
+	public void setDataCompra(Date dataCompra) {
+		this.dataCompra = dataCompra;
+	}
 
-  public String toString() {
-    return transactionId + " em " + dataCompra + " [" + produtos + "] via " + metodoPagamento;
-  }
+	/**
+	* Returns value of metodoPagamento
+	* @return
+	*/
+	public String getMetodoPagamento() {
+		return metodoPagamento;
+	}
+
+	/**
+	* Sets new value of metodoPagamento
+	* @param
+	*/
+	public void setMetodoPagamento(String metodoPagamento) {
+		this.metodoPagamento = metodoPagamento;
+	}
+
+	/**
+	* Returns value of transactionId
+	* @return
+	*/
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	/**
+	* Sets new value of transactionId
+	* @param
+	*/
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	/**
+	* Returns value of registro
+	* @return
+	*/
+	public Registro getRegistro() {
+		return registro;
+	}
+
+	/**
+	* Sets new value of registro
+	* @param
+	*/
+	public void setRegistro(Registro registro) {
+		this.registro = registro;
+	}
+
+	/**
+	* Create string representation of Pedido for printing
+	* @return
+	*/
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", produtos=" + produtos + ", dataCompra=" + dataCompra + ", metodoPagamento=" + metodoPagamento + ", transactionId=" + transactionId + ", registro=" + registro + "]";
+	}
 }
