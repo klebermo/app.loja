b5c4bb5e798ff4ab78b1b413e442b05edbf997dd
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-28 19:43:49 -0300

    Update MailConfig.java

--- src/main/java/org/loja/MailConfig.java
+++ src/main/java/org/loja/MailConfig.java
@@ -8,6 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+import java.util.Collections;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 public class MailConfig {
@@ -31,5 +38,37 @@ public class MailConfig {
       props.put("mail.debug", "true");
 
       return mailSender;
+  }
+
+  @Bean
+  public TemplateEngine emailTemplateEngine() {
+      final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+      templateEngine.addTemplateResolver(htmlTemplateResolver());
+      templateEngine.addTemplateResolver(stringTemplateResolver());
+      return templateEngine;
+  }
+
+  private ITemplateResolver htmlTemplateResolver() {
+      final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+      templateResolver.setOrder(Integer.valueOf(1));
+      templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
+      templateResolver.setPrefix("/email/");
+      templateResolver.setSuffix(".html");
+      templateResolver.setTemplateMode(TemplateMode.HTML);
+      templateResolver.setCharacterEncoding("UTF8");
+      templateResolver.setCacheable(false);
+      return templateResolver;
+  }
+
+  private ITemplateResolver stringTemplateResolver() {
+      final StringTemplateResolver templateResolver = new StringTemplateResolver();
+      templateResolver.setOrder(Integer.valueOf(2));
+      templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
+      //templateResolver.setPrefix("/email/");
+      //templateResolver.setSuffix(".html");
+      templateResolver.setTemplateMode(TemplateMode.HTML);
+      //templateResolver.setCharacterEncoding("UTF8");
+      templateResolver.setCacheable(false);
+      return templateResolver;
   }
 }
