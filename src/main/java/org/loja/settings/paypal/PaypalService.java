135e67ec6a94137da3de7b252b0aa0e4e2737ac8
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-29 06:13:35 -0300

    Update PaypalService.java

--- src/main/java/org/loja/settings/paypal/PaypalService.java
+++ src/main/java/org/loja/settings/paypal/PaypalService.java
@@ -155,7 +155,7 @@ public class PaypalService extends org.loja.settings.Service<Paypal> {
 
     String returnUrl = "/pedido/" + pedido.getId().toString();
     try {
-      mailSender.sendHTMLMessage("kleber-mota@uol.com.br", novo.getEmail(), "Confirmação de pedido realizado", "email/order", pedido, new java.util.Locale("pt", "br"));
+      mailSender.sendHTMLMessage(cliente.getUsuario().getEmail(), "Confirmação de pedido realizado", "email/order", pedido, new java.util.Locale("pt", "br"));
     } catch (Exception e) {
       e.printStackTrace();
     }
