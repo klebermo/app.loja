d921379fce19e9ccf06c48322c2fc18d78dd86e1
Kleber Oliveira <klebermo@gmail.com>
2020-05-05 11:29:32 -0300

    js scripts

--- src/main/resources/static/js/script-analytics.js
+++ src/main/resources/static/js/script-analytics.js
@@ -1,1 +1,7 @@
-// TODO:
+var codeName = navigator.appCodeName;
+var name = navigator.appName;
+var appVersion = navigator.appVersion;
+var cookieEnabled = navigator.cookieEnabled;
+var language = navigator.language;
+var platform = navigator.platform;
+var userAgent = navigator.userAgent;
